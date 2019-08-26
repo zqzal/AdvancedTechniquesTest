e188de31ce778d72bed67e2ec47ce60aaa7419d5
zqzal <936952020@qq.com>
2019-08-26 13:38:28 +0800

    MyApplication

--- app/src/main/java/com/xuxi/advancedtechniquestest/MainActivity.java
+++ app/src/main/java/com/xuxi/advancedtechniquestest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyApplication.getContext();//全局获取Context的机制
     }
 }
